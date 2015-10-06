1beba769ad703e9f31265e1be03327a0c1b76bd3
Devin Pastoor <devin.pastoor@gmail.com>
2015-10-06 14:36:26 -0400

    add simple table

--- shinyApp.R
+++ shinyApp.R
@@ -1,7 +1,11 @@
 library(shiny)
 
-ui <- fluidPage()
+ui <- fluidPage(
+  tableOutput('table1')
+)
+
 server <- function(input, output) {
+  output$table1 <- renderTable(Theoph)
 }
 
 shinyApp(ui = ui, server = server)
