5fdd2847c651724f3811640fbea47ab5b1971209
Devin Pastoor <devin.pastoor@gmail.com>
2015-10-06 16:06:37 -0400

    step 4: add input slider

--- shinyApp.R
+++ shinyApp.R
@@ -3,11 +3,12 @@ library(ggplot2)
 
 ui <- fluidPage(
   plotOutput('conctime'),
+  sliderInput("num_rows",label = "number of rows", min = 1, max = 10, value = 5),
   tableOutput('table1')
 )
 
 server <- function(input, output) {
-  output$table1 <- renderTable(Theoph)
+  output$table1 <- renderTable(head(Theoph, n = input$num_rows))
   
   output$conctime <- renderPlot({
     ggplot(Theoph, aes(x = Time, y = conc, group = Subject)) +
