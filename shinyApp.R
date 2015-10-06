97e5ddd08830667e2788c582a4fcf14c776d2178
Devin Pastoor <devin.pastoor@gmail.com>
2015-10-06 16:14:21 -0400

    revert back to no plot

--- shinyApp.R
+++ shinyApp.R
@@ -1,19 +1,12 @@
 library(shiny)
-library(ggplot2)
 
 ui <- fluidPage(
-  plotOutput('conctime'),
   sliderInput("num_rows",label = "number of rows", min = 1, max = 10, value = 5),
   tableOutput('table1')
 )
 
 server <- function(input, output) {
   output$table1 <- renderTable(head(Theoph, n = input$num_rows))
-  
-  output$conctime <- renderPlot({
-    ggplot(Theoph, aes(x = Time, y = conc, group = Subject)) +
-    geom_line() + theme_bw()
-    })
 }
 
 shinyApp(ui = ui, server = server)
