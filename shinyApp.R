9b446c1adac7f584913a506ea6ce1a77740f2418
Devin Pastoor <devin.pastoor@gmail.com>
2015-10-06 14:56:43 -0400

    step3: add plot

--- shinyApp.R
+++ shinyApp.R
@@ -1,11 +1,19 @@
 library(shiny)
+library(ggplot2)
 
 ui <- fluidPage(
+  plotOutput('conctime'),
   tableOutput('table1')
 )
 
 server <- function(input, output) {
   output$table1 <- renderTable(Theoph)
+  
+  output$conctime <- renderPlot({
+    ggplot(Theoph, aes(x = Time, y = conc, group = Subject)) +
+    geom_line() + theme_bw()
+    })
 }
 
 shinyApp(ui = ui, server = server)
+
